a151a842a1eaa4b04a2233d8317edce234fa71b5
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-07-05 14:54:35 -0400

    Implement configuration

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const snoowrap = require("snoowrap");
+const { NoCredentialsError } = require("snoowrap/dist/errors.js");
 const { CommentStream } = require("snoostorm");
 
 const { version } = require("../package.json");
 
 const debug = require("debug");
 const log = debug("reddit-comment-relayer:main");
+const clog = debug("reddit-comment-relayer:config");
+
+const cosmiconfig = require("cosmiconfig");
+const explorer = cosmiconfig("reddit-comment-relayer");
+
+const merge = require("merge-deep");
 
 /**
  * Starts the bot.
@@ -13,16 +20,22 @@ const log = debug("reddit-comment-relayer:main");
 function start(config) {
 	log("starting the bot");
 
-	const client = new snoowrap({
-		clientId: process.env.RCR_CLIENT_ID,
-		clientSecret: process.env.RCR_CLIENT_SECRET,
-		username: process.env.RCR_USERNAME,
-		password: process.env.RCR_PASSWORD,
-		...config.credentials,
-		userAgent: "Reddit Comment Relayer v" + version,
-	});
+	let client = {};
+	try {
+		client = new snoowrap({
+			...config.credentials,
+			userAgent: "Reddit Comment Relayer v" + version,
+		});
+	} catch (error) {
+		if (error instanceof NoCredentialsError) {
+			clog("missing credentials in config; see https://not-an-aardvark.github.io/snoowrap/snoowrap.html#snoowrap__anchor for possible values");
+		} else {
+			log("could not start snoowrap client: %o", error);
+		}
+		return;
+	}
 
-	const outputSub = client.getSubreddit(config.outputSubreddit || "test");
+	const outputSub = client.getSubreddit(config.outputSubreddit);
 
 	const stream = new CommentStream(client);
 	stream.on("item", comment => {
@@ -31,8 +44,37 @@ function start(config) {
 
 		outputSub.submitSelfPost({
 			text: config.outputPostTemplate || comment.body,
-			title: config.outputPostTitle || "New post",
+			title: config.outputPostTitle,
 		});
 	});
 }
-start({});
+
+/**
+ * Gets the parsed config using cosmiconfig, and starts the bot.
+ * @returns {Promise} A promise resolving to the resolved config.
+ */
+function getConfigAndStart() {
+	return explorer.search().then(result => {
+		clog("using the config at %s", result.filepath);
+
+		const config = merge({
+			credentials: {},
+			includeCommentAuthors: [],
+			includeSubreddits: [],
+			outputPostTemplate: "",
+			outputPostTitle: "New post",
+			outputSubreddit: "test",
+		}, result.config);
+		clog("final config is %O", config);
+
+		try {
+			start(config);
+			return config;
+		} catch (error) {
+			log("bot error: %s", error.message);
+		}
+	}).catch(error => {
+		clog("could not get the config: %s", error.message);
+	});
+}
+getConfigAndStart();
